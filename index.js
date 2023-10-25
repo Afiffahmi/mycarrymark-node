3ace2cfc864be6acaac3a55cb4d12297d97cba5a
Afif Fahmi <afif.rafpi@gmail.com>
2023-10-26 00:45:01 +0800

    update index
    adding cors , so frontend can fetch the data from this server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import mcmRoute from "./src/mcmRoute.js"
-import authRoute from "./src/authRoute.js"
+import mcmRoute from "./src/mcmRoute.js";
+import authRoute from "./src/authRoute.js";
+import cors from 'cors';
 
-const port = 3000;
+const port = 5555;
 const app = express();
+app.use(cors());
 app.search(express.json());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -14,7 +16,6 @@ app.use("/auth", authRoute);
 app.get("/", (request, response) => {
   return response.status(200).send("Hi, there!, nice to meet you.");
 });
-
 
 app.listen(port, () => {
   console.log(`we re listening on port ${port}`);
