c1f9ab78564c8775ba80f0ba7246396b1af60b61
Afif Fahmi <afif.rafpi@gmail.com>
2023-11-28 04:04:35 +0800

    Update authRoute.js

--- src/authRoute.js
+++ src/authRoute.js
@@ -7,7 +7,7 @@ import {
 } from "firebase/auth";
 import {storage} from './firebase.js'; 
 import multer from "multer";
-import { getDownloadURL,uploadBytesResumable,ref } from "firebase/storage";
+import { getDownloadURL,uploadBytesResumable,ref, getStorage,listAll} from "firebase/storage";
 
 const app = express();
 const router = express.Router();
@@ -76,29 +76,68 @@ router.post("/signup", async (request, response) => {
     });
   })
 
-  router.post("/upload", upload.single("filename"),async (req,res)=> {
+  router.post("/:id/upload", upload.single("filename"),async (req,res)=> {
     try{
-
-      const storageRef = ref(storage,`files/${req.file.originalname}`)
+      const user = req.params.id
+      const storageRef = ref(storage,`files/user/${user}/${req.file.originalname}`)
 
       const metadata = {
         contentType : req.file.mimetype,
       };
       const snapshot = await uploadBytesResumable(storageRef, req.file.buffer,metadata);
       const downloadURL = await getDownloadURL(snapshot.ref);
-
+      
       console.log("file successfully uploaded")
       return res.send({
         message: 'file uploaded',
         name: req.file.originalname,
         type: req.file.mimetype,
-        downloadURL : downloadURL
+        downloadURL : downloadURL,
+        owner : user
       })
     }catch(e){
-      return res.status(500).send(e);
+      return res.status(500).send(console.log(e));
     }
   })
 
+
+//get list of files
+router.get("/:id/files", async (request, response) => {
+  const storage = getStorage();
+
+  try {
+    const listRef = ref(storage, `files/user/${request.params.id}`);
+
+    // Find all the prefixes and items.
+    listAll(listRef)
+      .then(async (res) => {
+        let results = [];
+        res.prefixes.forEach((folderRef) => {
+          // All the prefixes under listRef.
+          // You may call listAll() recursively on them.
+          results.push({ type: 'folder', name: folderRef.name });
+        });
+
+        // Fetch download URLs for all items
+        const itemPromises = res.items.map(async (itemRef) => {
+          const downloadURL = await getDownloadURL(itemRef);
+          results.push({ type: 'item', name: itemRef.name, downloadURL : downloadURL });
+        });
+
+        // Wait for all download URLs to be fetched
+        await Promise.all(itemPromises);
+
+        // Send the results as the response
+        return response.status(200).send(results);
+      })
+      .catch((error) => {
+        // Uh-oh, an error occurred!
+        return response.status(500).send(error);
+      });
+  } catch (e) {
+    return response.status(500).send(e);
+  }
+});
 
 
  
