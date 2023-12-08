1711dad37bd1636f998da7f0bf91cb08765da496
Afif Fahmi <afif.rafpi@gmail.com>
2023-12-09 05:48:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,21 +24,8 @@ app.use(favicon(path.join(__dirname,'public','favicon.ico')))
 app.use("/carrymark", mcmRoute);
 app.use("/auth", authRoute);
 app.use("/class",classRoute);
-app.use(express.static('public'))
 
 app.engine('html',ejs.renderFile);
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
 app.get("/", (request, response) => {
   response.render('index.html')
