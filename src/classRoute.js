c00a129b8f2c9397a2095e7788ebdc578ef30b51
Afif Fahmi <afif.rafpi@gmail.com>
2023-12-10 22:14:26 +0800

    Update classRoute.js

--- src/classRoute.js
+++ src/classRoute.js
@@ -238,33 +238,29 @@ router.get("/:id/forum", async (request, response) => {
     // Get the forum data
     const forumSnapshot = await getDocs(collection(db, `class/${classId}/forum`));
     let forum = [];
+    let messages = [];
+    const forumData = forumSnapshot.docs.map(doc => {
 
-    for (let doc of forumSnapshot.docs) {
-      let messages = [];
-      const forumId = doc.id;
-
-      // Get the messages data
-      const messageSnapshot = await getDocs(collection(db, `class/${classId}/forum/${forumId}/messages`));
-
-      messageSnapshot.docs.forEach(messageDoc => {
-        messages.push({
-          id: messageDoc.id,
-          ...messageDoc.data(),
-        });
+      if(forumData){
+        const messageSnapshot = getDocs(collection(db, `class/${classId}/forum/${forum.id}/messages`));
+    
+    const messageData = messageSnapshot.docs.map(doc => {
+      messages.push({
+  
+          id: doc.id,
+          ...doc.data(),
       });
+    });
 
+      }
       forum.push({
-        id: forumId,
+        id: doc.id,
         messages: messages,
         ...doc.data(),
       });
+      
     }
-
-    response.json(forum);
-  } catch (error) {
-    response.status(500).send(error.message);
-  }
-});
+    );
     
     
 
