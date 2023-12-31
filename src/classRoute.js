9f8989c1d9c1ab561ee9fd17d62b11a9e669424b
Afif Fahmi <afif.rafpi@gmail.com>
2024-01-01 01:51:54 +0800

    Update classRoute.js

--- src/classRoute.js
+++ src/classRoute.js
@@ -631,7 +631,7 @@ router.get("/:id/bad-performance", async (request, response) => {
       // Calculate the total weighted grade for the specific student
       const totalWeightedGrade = courseworkSnapshot.docs.reduce((sum, coursework) => {
         const grade = grades.find(grade => grade.courseworkId === coursework.data().courseworkId);
-        return sum + (grade ? Number(grade.grade) * Number(coursework.data().weight) : 0);
+        return sum + (grade ? Number(grade.grade) * Number(coursework.data().weighted) : 0);
       }, 0);
 
       // Find the student details for the specific student
